e46a62d3a2996a8bd00b196540a86a0ba861ec41
TomaszSkibinski <45923192+TomaszSkibinski@users.noreply.github.com>
2018-12-26 13:18:55 +0100

    Add files via upload
    JavaCourseLesson1Q4

---
+++ ProblemSetLesson1Q4.java
@@ -0,0 +1,15 @@
+
+public class ProblemSetLesson1Q4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double A = 2.0;
+int B = 5;
+int C = 2;
+double D = A * (B/C);
+System.out.println(D);
+
+
+	}
+
+}
