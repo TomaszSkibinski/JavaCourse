26c60e788506968faef70fbae8505294113cd50a
Tomasz Skibinski <tomasz.skibinsk@gmail.com>
2018-12-26 13:04:05 +0100

    1 commit Java Course

---
+++ ProblemSetLesson1Q3.java
@@ -0,0 +1,14 @@
+
+public class ProblemSetLesson1Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int BankBalance = 500;
+	BankBalance = BankBalance + 250;
+	BankBalance = BankBalance - 100;
+	System.out.println(BankBalance);
+	
+	}
+
+}
