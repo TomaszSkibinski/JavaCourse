471ee65ab8b9cd5d0e27082347a15003c328a35d
TomaszSkibinski <45923192+TomaszSkibinski@users.noreply.github.com>
2018-12-26 13:47:50 +0100

    Add files via upload
    JavaCourseLesson1Q7

---
+++ ProblemSetLesson1Q7.java
@@ -0,0 +1,12 @@
+
+public class ProblemSetLesson1Q7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double fehrenheit = 60.0;
+double celcius;
+celcius = (fehrenheit - 32) * 5/9;
+System.out.println(celcius);
+	}
+
+}
