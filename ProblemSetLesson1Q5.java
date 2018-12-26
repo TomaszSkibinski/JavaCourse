ca82a5b345998decafc5acab96ea225dd5a0b55c
TomaszSkibinski <45923192+TomaszSkibinski@users.noreply.github.com>
2018-12-26 13:27:34 +0100

    Add files via upload
    JavaCourseLesson1Q5

---
+++ ProblemSetLesson1Q5.java
@@ -0,0 +1,12 @@
+
+public class ProblemSetLesson1Q5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int day = 21;
+String month = "February";
+System.out.println(day + " " + month);;
+
+	}
+
+}
