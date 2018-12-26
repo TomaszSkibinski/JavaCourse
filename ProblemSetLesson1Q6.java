03948cc36948e3a4c8da8cd860e59895e9e80506
TomaszSkibinski <45923192+TomaszSkibinski@users.noreply.github.com>
2018-12-26 13:37:37 +0100

    Add files via upload
    JavaCourseLesson1Q6

---
+++ ProblemSetLesson1Q6.java
@@ -0,0 +1,15 @@
+
+public class ProblemSetLesson1Q6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String FirstName = "Tomasz";
+String LastName = "Skibinski";
+String FullName = FirstName + " " + LastName;
+System.out.println("My name is " + FullName);
+
+int NumberOfLetters = FirstName.length() + LastName.length();
+System.out.println("My full name contains" + " " + NumberOfLetters + " " + "letters");
+	}
+
+}
