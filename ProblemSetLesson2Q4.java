940fa458313dac56db89b9dffa306db1a3e5450c
TomaszSkibinski <45923192+TomaszSkibinski@users.noreply.github.com>
2018-12-27 21:46:07 +0100

    Add files via upload
    ProblemSolvingLesson2Q4

---
+++ ProblemSetLesson2Q4.java
@@ -0,0 +1,18 @@
+
+public class ProblemSetLesson2Q4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+boolean isSnowing = false;
+boolean isRaining = true;
+double temperature = 60.0;
+
+if (isSnowing || isRaining || temperature < 50.0) {
+	System.out.println("Stay at home");
+}
+else {
+	System.out.println("Let's go out");
+}
+	}
+
+}
