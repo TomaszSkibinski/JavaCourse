1cb240636f253257b9389029da79c4da78e6641e
TomaszSkibinski <45923192+TomaszSkibinski@users.noreply.github.com>
2018-12-28 10:54:52 +0100

    Add files via upload
    Lesson2Q6

---
+++ ProblemSetLesson2Q6.java
@@ -0,0 +1,29 @@
+
+public class ProblemSetLesson2Q6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int day = 1;
+		boolean holiday = false;
+		/* Monday = 1;
+		int Tuesday = 2;
+		int Wednesday = 3;
+		int Thursday = 4;
+		int Friday = 5;
+		int Saturday = 6;
+		int Sunday = 7;
+		*/
+		if (day >= 1 && day <= 5 &&  !holiday) {
+			System.out.println("Wake up");
+		}
+		else {
+			System.out.println("Sleep");
+		}
+		
+		}
+		
+		
+	}
+
+
