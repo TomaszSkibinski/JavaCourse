b23f14fb5ef88e2835a910e1091031110683bc3f
TomaszSkibinski <45923192+TomaszSkibinski@users.noreply.github.com>
2018-12-28 10:15:55 +0100

    Add files via upload
    Error at range 5-12. Program prints morning night. Why?

---
+++ ProblemSetLesson2Q5.java
@@ -0,0 +1,24 @@
+
+public class ProblemSetLesson2Q5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int time = 24;
+		
+		
+		if (time >= 5 && time < 12 ) {
+			System.out.println ("morning");
+		
+		}
+		
+		if (time >= 12 && time < 20 ) {
+			System.out.println ("daytime");
+		
+	}
+
+		else {
+			System.out.println("night");
+}
+}
+}
